3052575698ced8a84a082f90538144847c171d91
Elvis Duru <duruelvis95@gmail.com>
2019-06-19 13:44:34 +0100

    debuggin nkechi acct

--- controllers/users.js
+++ controllers/users.js
@@ -881,6 +881,7 @@ async function userMatrix(req, res) {
         fullname: 1,
         _id: 0
       });
+    console.log(rootUser);
     const rootUser = rootUserArr[0];
     var image = "";
     if (rootUser.rank.startsWith("SilverLife"))
